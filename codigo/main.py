9dd1b58660b1a4d9363631f2062e00bce6cb18cc
jcbermudec <104237125+jcbermudec@users.noreply.github.com>
2022-11-07 16:09:42 -0500

    Update main.py

--- codigo/main.py
+++ codigo/main.py
@@ -1,3 +1,5 @@
+from math import sqrt
+from turtle import color
 import pandas as pd
 import networkx as nx
 import geopandas as gpd
@@ -29,10 +31,12 @@ def create_graph():
         double_via = df.iloc[i]["oneway"]
         risk = df.iloc[i]["harassmentRisk"]
         geometry = df.iloc[i]["geometry"]
-        new_weight = distance * (risk * 10)
-        G.add_edge(v1, v2, weight=new_weight, geometry=geometry, old_distance=distance)
+        weight1 = distance * (risk * 10)
+        weight2 = 5 * distance + (risk * 100)
+        weight3 = sqrt(risk * risk + distance * distance)
+        G.add_edge(v1, v2, weight1=weight1, weight2=weight2, weight3=weight3, geometry=geometry, risk=risk, old_distance=distance)
         if double_via:
-            G.add_edge(v2, v1, weight=new_weight, geometry=geometry, old_distance=distance)
+            G.add_edge(v2, v1, weight1=weight1, weight2=weight2, weight3=weight3, geometry=geometry, risk=risk, old_distance=distance)
 
     return G
 
@@ -43,14 +47,17 @@ def print_graph(G):
         print(i)
         print(keys + " connected to")
         for value in values:
-            print("=> " + value + ": {'weight': " + str(values[value]["weight"]) + "}"+ ", {'original weight': " + str(values[value]["old_distance"]) + "}")
+            print("=> " + value + ": {'weight1': " + str(values[value]["weight1"]) + "}"+
+            ", {'weight2': " + str(values[value]["weight2"]) + "}"+
+            ", {'weight3': " + str(values[value]["weight3"]) + "}"+
+            ", {'original weight': " + str(values[value]["old_distance"]) + "}")
         print("")
         i += 1
 
 def print_path(G):
     d = nx.to_dict_of_dicts(G)
 
-    starting_node = input("Write the starting node: ")
+    starting_node = input("\nWrite the starting node: ")
 
     while starting_node not in d.keys():
         starting_node = input("\nWrite a valid starting node: ")
@@ -67,26 +74,72 @@ def print_path(G):
         while destination_node not in d.keys():
             destination_node = input("\nWrite a valid destination node: ")
 
-    path = dijkstra.dijkstra_path(G, starting_node, destination_node)
-    cost = dijkstra.dijkstra_path_length(G, starting_node, destination_node)
+    path1 = dijkstra.dijkstra_path(G, starting_node, destination_node, "weight1")
 
-    print("\nThe path found is:\n")
+    path2 = dijkstra.dijkstra_path(G, starting_node, destination_node, "weight2")
+
+    path3 = dijkstra.dijkstra_path(G, starting_node, destination_node, "weight3")
+
+    path_geometry1 = []
+    path_geometry2 = []
+    path_geometry3 = []
+
+    distance1 = 0
+    distance2 = 0
+    distance3 = 0
     
-    print(" -> ".join(path))
-    print("\nCost: " + str(cost)+"\n")
+    risk1 = 0 
+    risk2 = 0 
+    risk3 = 0
 
-    path_geometry = []
+    for i in range(len(path1)-1):
+        path_geometry1.append(G[path1[i]][path1[i+1]]["geometry"])
+        distance1 += G[path1[i]][path1[i+1]]["old_distance"]
+        risk1 += G[path1[i]][path1[i+1]]["risk"]
+    risk1 /= len(path1)-1
 
-    for i in range(len(path)-1):
-        path_geometry.append(G[path[i]][path[i+1]]["geometry"])
+    for i in range(len(path2)-1):
+        path_geometry2.append(G[path2[i]][path2[i+1]]["geometry"])
+        distance2 += G[path2[i]][path2[i+1]]["old_distance"]
+        risk2 += G[path2[i]][path2[i+1]]["risk"]
+    risk2 /= len(path2)-1
 
-    return [starting_node, destination_node, path_geometry]
+    for i in range(len(path3)-1):
+        path_geometry3.append(G[path3[i]][path3[i+1]]["geometry"])
+        distance3 += G[path3[i]][path3[i+1]]["old_distance"]
+        risk3 += G[path3[i]][path3[i+1]]["risk"]
+    risk3 /= len(path3)-1
+
+    print("\nThe paths found are:\n")
+
+    print("\nPATH 1\n"+" -> ".join(path1))
+    print("\nDistance: " + str(distance1)+"\n")
+    print("\nRisk: " + str(risk1)+"\n")
+
+    print("\nPATH 2\n"+" -> ".join(path2))
+    print("\nDistance: " + str(distance2)+"\n")
+    print("\nRisk: " + str(risk2)+"\n")
+
+    print("\nPATH 3\n"+" -> ".join(path3))
+    print("\nDistance: " + str(distance3)+"\n")
+    print("\nRisk: " + str(risk3)+"\n")
+
+    return [starting_node, destination_node, path_geometry1, path_geometry2, path_geometry3]
     
-def generate_plot(starting_node, destination_node, path_geometry):
+def generate_plot(starting_node, destination_node, path_geometry1, path_geometry2, path_geometry3):
 
-    path_geometry = pd.DataFrame(path_geometry, columns =['geometry'])
-    path_geometry['geometry'] = path_geometry['geometry'].apply(wkt.loads)
-    path_geometry = gpd.GeoDataFrame(path_geometry)
+    path_geometry1 = pd.DataFrame(path_geometry1, columns =['geometry'])
+    path_geometry1['geometry'] = path_geometry1['geometry'].apply(wkt.loads)
+    path_geometry1 = gpd.GeoDataFrame(path_geometry1)
+
+    path_geometry2 = pd.DataFrame(path_geometry2, columns =['geometry'])
+    path_geometry2['geometry'] = path_geometry2['geometry'].apply(wkt.loads)
+    path_geometry2 = gpd.GeoDataFrame(path_geometry2)
+
+    path_geometry3 = pd.DataFrame(path_geometry3, columns =['geometry'])
+    path_geometry3['geometry'] = path_geometry3['geometry'].apply(wkt.loads)
+    path_geometry3 = gpd.GeoDataFrame(path_geometry3)
+
     area = pd.read_csv('poligono_de_medellin.csv',sep=';')
     area['geometry'] = area['geometry'].apply(wkt.loads)
     area = gpd.GeoDataFrame(area)
@@ -98,17 +151,19 @@ def generate_plot(starting_node, destination_node, path_geometry):
     fig, ax = plt.subplots(figsize=(12,8))
     edges.plot(ax=ax, linewidth=0.3, missing_kwds={'color': 'dimgray'})
     area.plot(ax=ax, facecolor='black')
-    path_geometry.plot(ax=ax,linewidth=2, cmap = 'hsv')
+    path_geometry1.plot(ax=ax,linewidth=2, cmap="hsv")
+    path_geometry2.plot(ax=ax,linewidth=2, cmap="cool")
+    path_geometry3.plot(ax=ax,linewidth=2, cmap="copper")
     
-    plt.title("Safe and shortest route from " + starting_node + " to " + destination_node)
+    plt.title("Safe and shortest routes from " + starting_node + " to " + destination_node)
     plt.tight_layout()
-    plt.savefig(starting_node + "_to_" + destination_node + ".png")
+    plt.savefig("(3 paths) " + starting_node + "_to_" + destination_node + ".png")
 
 def main():
     G = create_graph()
-    print_graph(G)
+    #print_graph(G)
     inputs_plot = print_path(G)
-    generate_plot(inputs_plot[0], inputs_plot[1], inputs_plot[2])
+    generate_plot(inputs_plot[0], inputs_plot[1], inputs_plot[2], inputs_plot[3], inputs_plot[4])
 
 
 main()
